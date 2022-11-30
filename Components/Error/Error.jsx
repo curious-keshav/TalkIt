5236051a0f53b2a3ab8c0dd77fd095446b450b10
curious-keshav <keshavverma532@gmail.com>
2022-11-30 17:01:34 +0530

    first commit

---
+++ Components/Error/Error.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+//INTERNAL IMPORT
+import Style from "./Error.module.css";
+
+const Error = ({ error }) => {
+  return (
+    <div className={Style.Error}>
+      <div className={Style.Error_box}>
+        <h1>Reload the Browser</h1>
+        {error}
+      </div>
+    </div>
+  );
+};
+
+export default Error;
